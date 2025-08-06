7a1abe2d6a133348f91cf478b116a7ec296963e4
Angel Mejia <130812286+AngelMM26@users.noreply.github.com>
2025-08-06 01:42:40 -0400

    Delete search.js
    Duplicate file

--- search.js
+++
@@ -1,8 +0,0 @@
-const inputElement = document.getElementById("userInput");
-
-inputElement.addEventListener("keydown", function (event) {
-    if (event.key === "Enter" && inputElement.value !== "") {
-        const query = encodeURIComponent(inputElement.value);
-        window.location.href = `serp.html?q=${query}`;
-    }
-});
